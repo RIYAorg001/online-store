51d658dbeb581a00b4f912a26882b905561465ee
Riya Chowdhury <r.chowdhury@zensar.com>
2022-05-03 03:07:10 +0530

    my commit

---
+++ src/app/store/market.ts
@@ -0,0 +1,25 @@
+import { Product } from "./product.model";
+
+export const PRODUCTS: Product[] = [
+    {
+      id: 0,
+      name: "HP Inspirion",
+      price: 700
+    },
+    {
+      id: 1,
+      name: "MacBook Pro 2018",
+      price: 15000
+    },
+    {
+      id: 2,
+      name: "Dell 5500",
+      price: 3000
+    },
+   {
+    id: 3,
+    name: "acer",
+    price: 1000
+  }, 
+
+]
